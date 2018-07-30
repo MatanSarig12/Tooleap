c6ab829fa82c107a1f32dbcc3fe16e3e82dda1b4
Ran Rafa <ranrafael.rayn@gmail.com>
2018-07-30 23:32:01 +0300

    free text for question difficulty

--- tooleap/quiz/urls.py
+++ tooleap/quiz/urls.py
@@ -5,16 +5,11 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    # ex: /polls/5/
-    path('<int:question_id>/', views.detail, name='detail'),
-    # ex: /polls/5/results/
-    path('<int:question_id>/results/', views.results, name='results'),
-    # ex: /polls/5/vote/
 
     path('course/<int:course_id>/questions', views.course_question, name='course_question'),
 
-    path('course/<int:course_id>/categories', views.course_categories, name='course_categories'),
+    path('course/<int:course_id>/build_quiz', views.quiz_builder, name='course_categories'),
 
     path('custom_quiz/<int:course_id>/', views.custom_quiz, name='custom_quiz'),
-
+    
 ]
--- tooleap/quiz/views.py
+++ tooleap/quiz/views.py
@@ -7,13 +7,6 @@ from .models import Question, Course, Answer, Category
 from django.views.decorators.csrf import csrf_exempt
 
 
-def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
-
-def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
-
 ##This generates html for questions for a given course.
 def course_question(request, course_id):
     course_questions_list = Question.objects.filter(course_id=course_id)
@@ -28,9 +21,9 @@ def course_question(request, course_id):
     return HttpResponse(template.render(context, request))
 
 @csrf_exempt
-def course_categories(request, course_id):
+def quiz_builder(request, course_id):
     course_categories = Category.objects.filter(course_id=course_id)
-    template = loader.get_template('quiz/course_categories.html')
+    template = loader.get_template('quiz/quiz_builder.html')
     context = {
             'course_categories': course_categories,
             'course_id': course_id,
@@ -52,13 +45,17 @@ def index(request):
 @csrf_exempt
 def custom_quiz(request, course_id):
     category = request.POST['category']
-    difficulty = request.POST['difficulty']
-    num_of_qs = request.POST['num_of_qs']
-
+    hard = int(request.POST['hard'])
+    medium = int(request.POST['medium'])
+    easy = int(request.POST['easy'])
+    total_number_of_questions = hard + medium + easy
     template = loader.get_template('quiz/custom_quiz.html')
     context = {
     'custom_category' : category,
-    'custom_difficulty' : difficulty,
-    'custom_num_of_qs' : num_of_qs,
+    'custom_hard' : hard,
+    'custom_medium' : medium,
+    'custom_easy' : easy,
+    'total_questions': total_number_of_questions,
+    'course_id': course_id,
 }
     return HttpResponse(template.render(context, request))
